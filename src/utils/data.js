15e1f95be8243b82a2e8435ab289bfda3e8b7ee6
yamdeng <yamdeng@gmail.com>
2019-02-21 10:56:36 +0900

    테이블 페이지 데이터 수정

--- src/utils/data.js
+++ src/utils/data.js
@@ -373,7 +373,11 @@ for (let index = 0; index < 30; index++) {
 
 data.imageScrollData = [];
 for (let index = 0; index < 30; index++) {
-    data.imageScrollData.push({ id: index + 1, name: '사진' + (index + 1) });
+    data.imageScrollData.push({
+        id: index + 1,
+        name: '사진' + (index + 1),
+        platform: index % 2 === 0 ? 'ios' : 'android'
+    });
 }
 
 /*
