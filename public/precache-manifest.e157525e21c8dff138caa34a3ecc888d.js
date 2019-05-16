1ec91b0d9c7783a5c140f07a861208f8c356d709
yamdeng <yamdeng@gmail.com>
2019-05-16 21:49:50 +0900

    update

---
+++ public/precache-manifest.e157525e21c8dff138caa34a3ecc888d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce4e0694254e4eca2606bd5daaa0b365",
+    "url": "/github.io/scg-paas-front-prototype/static/media/1.ce4e0694.JPG"
+  },
+  {
+    "revision": "9498ee421ab721173ceb",
+    "url": "/github.io/scg-paas-front-prototype/static/css/main.chunk.css"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/github.io/scg-paas-front-prototype/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "cfad8f99df1b17ecc892",
+    "url": "/github.io/scg-paas-front-prototype/static/js/1.chunk.js"
+  },
+  {
+    "revision": "3cce1466e3553623d02f",
+    "url": "/github.io/scg-paas-front-prototype/static/js/runtime~main.js"
+  },
+  {
+    "revision": "9498ee421ab721173ceb",
+    "url": "/github.io/scg-paas-front-prototype/static/js/main.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/github.io/scg-paas-front-prototype/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/github.io/scg-paas-front-prototype/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/github.io/scg-paas-front-prototype/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "cfad8f99df1b17ecc892",
+    "url": "/github.io/scg-paas-front-prototype/static/css/1.chunk.css"
+  },
+  {
+    "revision": "3cd89091f11ef8c0a15f30c6bfc6c53b",
+    "url": "/github.io/scg-paas-front-prototype/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d92267f3ae9c9d60783054efe821f92.js"
+  "/github.io/scg-paas-front-prototype/precache-manifest.e157525e21c8dff138caa34a3ecc888d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/github.io/scg-paas-front-prototype/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
