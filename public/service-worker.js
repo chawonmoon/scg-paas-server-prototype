af011d42a47c472ed85667dbd58864dfb7e264a0
yamdeng <yamdeng@gmail.com>
2019-05-24 16:11:53 +0900

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github.io/scg-paas-front-prototype/precache-manifest.e157525e21c8dff138caa34a3ecc888d.js"
+  "/precache-manifest.67ce63c0f3d00dca001e25daf0c006ec.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/github.io/scg-paas-front-prototype/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
