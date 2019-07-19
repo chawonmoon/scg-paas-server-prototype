95c4c7aa746e37118509e219ef7943e9dae7b60b
yamdeng <yamdeng@gmail.com>
2019-07-19 15:51:36 +0900

    .

--- src/routes/application.js
+++ src/routes/application.js
@@ -27,7 +27,7 @@ router.get('/health', function(req, res) {
             dbAlive = true;
         }
         res.send({
-            version: '2',
+            version: '3',
             serverAlive: true,
             dbAlive: dbAlive
         });
